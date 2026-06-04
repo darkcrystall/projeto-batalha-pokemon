eefec24170754a26ab6db995b93f9d40d9f92292
Eduarda C. <dossantosec@outlook.com>
2026-06-04 00:46:22 -0300

    feat: melhora tela inicial

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 const startBtn = document.getElementById("startBtn");
 const startScreen = document.getElementById("start");
 const audio = document.getElementById("battle-theme");
-
+/* ao iniciar o jogo */
 startBtn.addEventListener("click", () => {
   startScreen.classList.add("hidden");
   audio.play().catch(() => {});
